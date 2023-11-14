a39e41069b404c38eed8145fe11851477d8acc55
Marillia Dantas <marillia.dantas@hotmail.com>
2023-11-13 22:42:07 -0300

    first commit

---
+++ features/step_definitions/home.rb
@@ -0,0 +1,12 @@
+Dado('que eu acesse a pagina home da consultoria') do
+  home.load
+  sleep 5
+end
+
+Quando('clico no menu sobre') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Então('deverei ser redirecionado para a sessão sobre') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
